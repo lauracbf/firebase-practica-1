c1f048170c2d4013f84a13f5f45bd02b55b50506
Laura Barragán <53447565+lauracbf@users.noreply.github.com>
2020-05-09 14:37:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyA87a5JfZWcgDb_HlHIKR5-TDIF8i52uaI",
-    authDomain: "fir-usuarios-3375c.firebaseapp.com",
-    databaseURL: "https://fir-usuarios-3375c.firebaseio.com",
-    projectId: "fir-usuarios-3375c",
-    storageBucket: "fir-usuarios-3375c.appspot.com",
-    messagingSenderId: "772408234103",
-    appId: "1:772408234103:web:bdae0a46a8d99f9f177672"
+ //Add firebase config
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
